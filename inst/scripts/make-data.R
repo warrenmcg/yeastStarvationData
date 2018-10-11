08360395d41d72888d5185e21f05b73d03b8ed64
warrenmcg <warren-mcgee@fsm.northwestern.edu>
2018-10-11 14:58:19 -0500

    add sanity checks to the reproducibility script: + check from where the script is launched, so that it can work inside or outside the package's script directory + make sure all of the necessary packages are installed

--- inst/scripts/make-data.R
+++ inst/scripts/make-data.R
@@ -1,3 +1,40 @@
+## Check from where the user is launching the script
+
+curr_dir <- getwd()
+git_dir <- 'inst/scripts'
+
+if (grepl(git_dir, curr_dir)) {
+  data_dir <- '../../data'
+  extdata_dir <- '../extdata'
+  if (!dir.exists(data_dir) || !dir.exists(extdata_dir)) {
+    stop("Something went wrong with the expected directory structure. Are you ",
+         "running this script within a modified copy of this package?")
+  }
+  message("This script is being run from within the package's scripts directory. ",
+          "It will place all of the data files into the appropriate directories.")
+} else {
+  message("This script is being run outside of the package's scripts directory. ",
+          "It will place all of the data files into the current directory")
+  data_dir <- '.'
+  extdata_dir <- '.'
+}
+
+## Check that the necessary packages are available
+suppressMessages({
+  if (!requireNamespace("Biostrings")) {
+    stop("The 'Biostrings' package does not seem to be installed. Install it from Bioconductor.")
+  }
+  if (!requireNamespace("dplyr")) {
+    stop("The 'dplyr' package does not seem to be installed. Install it from CRAN.")
+  }
+  if (!requireNamespace("matrixStats")) {
+    stop("The 'matrixStats' package does not seem to be installed. Install it from CRAN.")
+  }
+  if (!requireNamespace("openxlsx")) {
+    stop("The 'openxlsx' package does not seem to be installed. Install it from CRAN.")
+  }
+})
+
 ## Step 1A: Download the Yeast Annotations and create the kallisto index
 main_url <- "ftp://ftp.ensemblgenomes.org/pub/release-37/fungi/fasta/schizosaccharomyces_pombe"
 cdna_url <- file.path(main_url, 'cdna/Schizosaccharomyces_pombe.ASM294v2.cdna.all.fa.gz')
@@ -14,7 +51,7 @@ all_rnas <- c(cdnas, ncrnas)
 Biostrings::writeXStringSet(all_rnas, 'ASM294v2.pombase.all.fa.gz', compress = TRUE)
 
 if (Sys.which('kallisto') == "") {
-  stop("kallisto is not part of your system's $PATH. ",
+  stop("kallisto does not seem to be part of your system's $PATH. ",
        "Please make sure it is installed and on your $PATH.")
 } else {
   v_string <- system2("kallisto", "version", stdout = TRUE)
@@ -25,13 +62,12 @@ system2('kallisto', args)
 
 ## Step 1B: Download the raw FASTQ files from ArrayExpress
 
-data_dir <- '../extdata'
 samples <- paste0('ERR13590', 6:9)
 url <- 'ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR135'
 sample_files <- paste0(samples, '.fastq.gz')
 url_files <- file.path(url, samples, sample_files)
 
-out_files <- file.path(data_dir, sample_files)
+out_files <- file.path(extdata_dir, sample_files)
 
 for (i in 1:length(samples)) {
   download.file(url_files[i], out_files[i])
@@ -41,7 +77,7 @@ for (i in 1:length(samples)) {
   ##   --fr-stranded -o {OUTPUT_FILE} {INPUT_FILE}
   args <- c('quant', '-i ASM294v2.pombase.all.kidx', '-b 100', '-l 200',
             '-s 20', '--single', '--fr-stranded',
-            paste('-o', file.path(data_dir, samples[i])),
+            paste('-o', file.path(extdata_dir, samples[i])),
             shQuote(out_files[i]))
   system2('kallisto', args)
   file.remove(out_files[i])
@@ -54,9 +90,10 @@ file.remove('ASM294v2.pombase.all.fa.gz')
 ## Step 3: Download the absolute count data from Lovell et al 2015
 
 url <- 'https://ars.els-cdn.com/content/image/1-s2.0-S0092867412011269-mmc1.xlsx'
-download.file(url, '../extdata/mmc1.xlsx')
-table <- openxlsx::read.xlsx('../extdata/mmc1.xlsx', sheet = 3, startRow = 2)
-file.remove('../extdata/mmc1.xlsx')
+mmc1_file <- file.path(extdata_dir, 'mmc1.xlsx')
+download.file(url, mmc1_file)
+table <- openxlsx::read.xlsx(mmc1_file, sheet = 3, startRow = 2)
+file.remove(mmc1_file)
 
 table <- dplyr::select(table, 1, 2, 11, 14, 17, 20)
 table$fold_change <- log2(rowMeans(as.matrix(table[,5:6])) / rowMeans(as.matrix(table[,3:4])))
@@ -76,9 +113,9 @@ denom_gene <- names(which(cov == min(cov[which(cov>0)], na.rm = T)))
 absCounts <- table
 colnames(absCounts) <- c("PombaseID", "GeneName", "Ctr1_CPC", "Ctr2_CPC",
   "noN2_1_CPC", "noN2_2_CPC", "fold_change")
-save(absCounts, file = '../../data/absCounts.rda')
+save(absCounts, file = file.path(data_dir, 'absCounts.rda'))
 denom <- annos$target_id[which(annos$gene_id == denom_gene)]
-save(denom, file = '../../data/denom.rda')
+save(denom, file = file.path(data_dir, 'denom.rda'))
 
 ## Step 4: Collect the metadata and yeast annotations necessary for sleuth
 ## Metadata
@@ -92,7 +129,7 @@ yeastS2C <- all_samples[grepl("pA$", all_samples$sample), ]
 yeastS2C$condition <- as.factor(yeastS2C$condition)
 levels(yeastS2C$condition) <- c('control', 'noN2')
 
-save(yeastS2C, file = '../../data/yeastS2C.rda')
+save(yeastS2C, file = file.path(data_dir 'yeastS2C.rda'))
 
 ## Yeast Annotations
 splits <- strsplit(names(all_rnas), split = " ", fixed = T)
@@ -116,4 +153,4 @@ data <- lapply(splits, function(x) {
 annos <- do.call(rbind, data)
 yeastAnnos <- as.data.frame(annos, stringsAsFactors = FALSE)
 
-save(yeastAnnos, file = '../../data/yeastAnnos.rda')
+save(yeastAnnos, file = file.path(data_dir, 'yeastAnnos.rda'))
